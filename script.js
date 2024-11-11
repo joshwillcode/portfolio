95699bd89f9dc5405d4d67731efe3833d11be855
Joshua Williams <72379703+joshwillcode@users.noreply.github.com>
2024-11-11 12:10:06 -0800

    Add scroll to section head on mousewheel

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ const revealNav = () => {
 }
 
 document.addEventListener('mousemove', revealNav);
+
+// scroll between sections with one mousewheel click
+
+let scroll = 1
+
+const scrollViewport = () => {
+    element = document.getElementById(`scroll-${scroll}`);
+    element.scrollIntoView(true);
+}
+
+const wheel = (event) => {
+    // scrolling up
+    if(event.deltaY < 0) {
+        if(scroll > 1) {
+            scroll--;
+            scrollViewport();
+        }
+    } 
+    // scrolling down
+    else {
+        if(scroll < 4) {
+            scroll++;
+            scrollViewport();
+        }
+    }
+}
+
+document.addEventListener('wheel', wheel);
